fd1d3dd66cd415a944639adf59c9cbc580a15e06
luliangyu <luliangyu@seewintech.com>
2021-08-18 14:18:57 +0800

    修改密钥对象为值对象

--- framework/src/main/java/com/byaoh/mall/framework/securt/Key.java
+++ framework/src/main/java/com/byaoh/mall/framework/securt/Key.java
@@ -1,37 +1,54 @@
 package com.byaoh.mall.framework.securt;
 
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
 
 import java.io.Serializable;
 
 /**
- * 密钥
+ * 密钥对象
  *
  * @author luliangyu
  * @date 2021-06-25 11:39
  */
-@Data
+@Getter
+@ToString
+@EqualsAndHashCode
 public class Key implements Serializable {
 
 	private static final long serialVersionUID = 4637201454606182907L;
 	/**
 	 * 加密算法
 	 */
-	private String algorithm;
+	private final String algorithm;
 	/**
 	 * 私钥字节文件
 	 */
-	private byte[] privateKey;
+	private final byte[] privateKey;
 	/**
 	 * 公钥字节文件
 	 */
-	private byte[] publicKey;
+	private final byte[] publicKey;
+
+	/**
+	 * 创建一个密钥对象
+	 *
+	 * @param algorithm  加密算法
+	 * @param privateKey 私钥
+	 * @param publicKey  公钥
+	 */
+	public Key(String algorithm, byte[] privateKey, byte[] publicKey) {
+		this.algorithm = algorithm;
+		this.privateKey = privateKey;
+		this.publicKey = publicKey;
+	}
 
 	public String privateKeyStr() {
-		return BaseCryptoStrategy.bytesToString(privateKey);
+		return BaseCryptoStrategy.bytesToString(getPrivateKey());
 	}
 
 	public String publicKeyStr() {
-		return BaseCryptoStrategy.bytesToString(publicKey);
+		return BaseCryptoStrategy.bytesToString(getPublicKey());
 	}
 }
